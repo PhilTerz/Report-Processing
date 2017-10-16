5e73e219ee2509fc3f0c22c9f4fbe2caec7f04be
Philip Terzic <PhilTerz@users.noreply.github.com>
2017-10-16 12:46:40 -0700

    Removed extraneous data
    Removed height, width, top, and left from data. Cleaned out some formatting issues from tabula

--- e1_parser.py
+++ e1_parser.py
@@ -1,25 +1,39 @@
 import json
 import tabula
+import pprint
 
-def e1_report_json:
+def e1_report_json():
     # Read pdf into json using tabula
     report = tabula.read_pdf("E1.pdf", output_format="json", pages="1")
     report_clean = []
-
     # @data: an array of arrays representings rows of the pdf
     # @obj: the object representations of each element in a row
     # Get rid of all the spaces created from tabula, noted where height = 0
     # Report is inconsistent, so standardize all '-' to '0' (they are equivalent in meaning)
-    for row in report[0]['data']:
-        for obj in row:
+    for j, row in enumerate(report[0]['data']):
+        row_clean = []
+
+        for index, obj in enumerate(row):
+            is_firstCol = False
             if obj['height'] != 0.0:
-                if obj['text'] == '-':
-                    obj['text'] = "0"
-                row_clean = []
-                row_clean.append(obj)
-                report_clean.append(row_clean)
-    e1_to_file(report_clean)
+                if index == 0 and j > 0:
+                    is_firstCol = True
+                    row_name = obj['text'].rsplit(' ', 1)[0]
+                    row_first_num = obj['text'].rsplit(' ', 1)[1]
+                    row_clean.extend([row_name, row_first_num])
+                else: 
+                    row_clean.append(obj['text'])
+    
+        report_clean.append(row_clean)
+
+        if j == 3: 
+            pp = pprint.PrettyPrinter(indent=2)
+            pp.pprint(report_clean)
+            return
+    
 
 def e1_to_file(report_clean):
     with open('tabtest.txt', 'w+') as outfile:
         json.dump(report_clean, outfile, sort_keys=True, indent=2, separators=(',', ': '))
+
+e1_report_json()
